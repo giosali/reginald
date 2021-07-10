3cfa47db9683143e0107dd156b4e99c685423ac4
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-07-09 23:37:36 -0500

    Add Settings model to facilitate process of handling user settings

---
+++ Reginald/Models/SettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+
+namespace Reginald.Models
+{
+    public class SettingsModel
+    {
+        public bool IsDarkModeEnabled { get; set; }
+        public Color SearchBackgroundColor { get; set; }
+        public Brush SearchDescriptionTextBrush { get; set; }
+        public Brush SearchAltTextBrush { get; set; }
+        public Brush SearchInputTextBrush { get; set; }
+        public Brush SearchInputCaretBrush { get; set; }
+    }
+}
