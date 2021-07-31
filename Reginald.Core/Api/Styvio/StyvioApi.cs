100a438755680a00529b9b1c96e8545729e8d48b
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-07-31 17:37:56 -0500

    Update handling for Styvio API

--- Reginald.Core/Api/Styvio/StyvioApi.cs
+++ Reginald.Core/Api/Styvio/StyvioApi.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Reginald.Core.Base;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Reginald.Core.Api.Styvio
@@ -21,7 +22,25 @@ namespace Reginald.Core.Api.Styvio
             }
             catch (HttpRequestException)
             {
-                return new StyvioStock();
+                return null;
+            }
+        }
+
+        public static async Task<StyvioStock> GetStock(string stock, CancellationToken token)
+        {
+            try
+            {
+                using (HttpClient client = new())
+                {
+                    HttpResponseMessage response = await client.GetAsync(string.Format(Constants.StyvioStockEpFormat, stock), token);
+                    response.EnsureSuccessStatusCode();
+                    StyvioStock styvioStock = JsonConvert.DeserializeObject<StyvioStock>(await response.Content.ReadAsStringAsync());
+                    return styvioStock;
+                }
+            }
+            catch (HttpRequestException)
+            {
+                return null;
             }
         }
     }
