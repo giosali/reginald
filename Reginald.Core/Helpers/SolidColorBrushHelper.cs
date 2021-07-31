b24a051d2f29909ccab7ccdde23b1366e4818aab
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-07-31 17:39:21 -0500

    Add helper for SolidColorBrush objects

---
+++ Reginald.Core/Helpers/SolidColorBrushHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace Reginald.Core.Helpers
+{
+    public static class SolidColorBrushHelper
+    {
+        public static SolidColorBrush FromRgb(System.Drawing.Color color)
+        {
+            return new SolidColorBrush(Color.FromRgb(color.R, color.G, color.B));
+        }
+
+        public static SolidColorBrush FromArgb(System.Drawing.Color color)
+        {
+            return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+    }
+}
