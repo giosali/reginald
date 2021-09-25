9bbb72e1d1bf3a20bf28a0f37b6d1092031b2d01
giosali <70548443+giosali@users.noreply.github.com>
2021-09-24 21:54:41 -0500

    Add converters

---
+++ Reginald/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Reginald.Converters
+{
+    [ValueConversion(typeof(Color), typeof(Brush))]
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = (Color)value;
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Reginald/Converters/ColorToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using Reginald.Core.Helpers;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Reginald.Converters
+{
+    [ValueConversion(typeof(Color), typeof(String))]
+    public class ColorToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = (Color)value;
+            _ = ColorHelper.TryGetName(color, out string name);
+            return name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
