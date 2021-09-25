8424130909ed2612723abdebe3bd191602d8772a
giosali <70548443+giosali@users.noreply.github.com>
2021-09-24 22:22:19 -0500

    Add helper functions for colors and brushes

---
+++ Reginald.Core/Helpers/ColorHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Windows.Media;
+
+namespace Reginald.Core.Helpers
+{
+    public static class ColorHelper
+    {
+        public static Color FromString(string expression)
+        {
+            return (Color)ColorConverter.ConvertFromString(expression);
+        }
+
+        public static bool TryGetName(Color color, out string name)
+        {
+            name = color.ToString();
+            PropertyInfo[] properties = typeof(Colors).GetProperties();
+            for (int i = 0; i < properties.Length; i++)
+            {
+                PropertyInfo property = properties[i];
+                string hex = property.GetValue(color).ToString();
+                if (name == hex)
+                {
+                    name = property.Name;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- Reginald.Core/Helpers/SolidColorBrushHelper.cs
+++ Reginald.Core/Helpers/SolidColorBrushHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Media;
+﻿using System.Reflection;
+using System.Windows.Media;
 
 namespace Reginald.Core.Helpers
 {
@@ -12,6 +13,33 @@ namespace Reginald.Core.Helpers
         public static SolidColorBrush FromArgb(System.Drawing.Color color)
         {
             return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        public static SolidColorBrush FromString(string expression)
+        {
+            return expression is null ? null : (SolidColorBrush)new BrushConverter().ConvertFromString(expression);
+        }
+
+        public static bool TryGetName(Brush brush, out string name)
+        {
+            name = null;
+            if (brush is not null)
+            {
+                name = brush.ToString();
+                PropertyInfo[] properties = typeof(Brushes).GetProperties();
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    PropertyInfo property = properties[i];
+                    string hex = property.GetValue(brush).ToString();
+                    if (name == hex)
+                    {
+                        name = property.Name;
+                        break;
+                    }
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
