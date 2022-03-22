7e68b101d425da23558a30cff2d4d03cd9a9b7ae
giosali <70548443+giosali@users.noreply.github.com>
2022-03-22 12:36:44 -0500

    Add native acrylic material

---
+++ Reginald/Utilities/AcrylicMaterial.cs
@@ -0,0 +1,63 @@
+﻿namespace Reginald.Utilities
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Windows.Media;
+    using static Reginald.Utilities.NativeMethods;
+
+    public class AcrylicMaterial
+    {
+        public AcrylicMaterial(IntPtr hWnd, byte opacity, Brush backgroundBrush)
+        {
+            Handle = hWnd;
+            Opacity = opacity;
+            BackgroundColorHex = ToBgr(backgroundBrush as SolidColorBrush);
+        }
+
+        public AcrylicMaterial(IntPtr hWnd, byte opacity, Color backgroundColor)
+        {
+            Handle = hWnd;
+            Opacity = opacity;
+            BackgroundColorHex = ToBgr(backgroundColor);
+        }
+
+        private IntPtr Handle { get; set; }
+
+        private uint Opacity { get; set; }
+
+        /// <summary>
+        /// Gets or sets an unsigned integer in BGR format as the background color of the acrylic material.
+        /// </summary>
+        private uint BackgroundColorHex { get; set; }
+
+        public void Enable()
+        {
+            AccentPolicy accent = new()
+            {
+                AccentState = AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND,
+                GradientColor = (Opacity << 24) | (BackgroundColorHex & 0xFFFFFF),
+            };
+            int accentStructSize = Marshal.SizeOf(accent);
+            IntPtr accentPtr = Marshal.AllocHGlobal(accentStructSize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+            WindowCompositionAttributeData data = new()
+            {
+                Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+                SizeOfData = accentStructSize,
+                Data = accentPtr,
+            };
+            _ = SetWindowCompositionAttribute(Handle, ref data);
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        private static uint ToBgr(SolidColorBrush brush)
+        {
+            return (uint)brush.Color.B << 16 | (uint)brush.Color.G << 8 | brush.Color.R;
+        }
+
+        private static uint ToBgr(Color color)
+        {
+            return (uint)color.B << 16 | (uint)color.G << 8 | color.R;
+        }
+    }
+}
