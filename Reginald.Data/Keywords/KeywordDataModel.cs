2c441dfe6f729036e8dda11e039814f03431d743
giosali <70548443+giosali@users.noreply.github.com>
2022-04-03 21:43:06 -0500

    Allow for easier access to data files in settings

---
+++ Reginald.Data/Keywords/KeywordDataModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Reginald.Data.Keywords
+{
+    /// <summary>
+    /// Represents the contents of a keyword JSON file to use with Newtonsoft.Json.
+    /// </summary>
+    public class KeywordDataModel : IKeywordDataModel
+    {
+        public string Guid { get; set; }
+
+        public string Name { get; set; }
+
+        public string Keyword { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Format { get; set; }
+
+        public string Placeholder { get; set; }
+
+        public string Caption { get; set; }
+
+        public bool IsEnabled { get; set; }
+
+        public string DisplayDescription { get; set; }
+
+        public string PrimaryIcon { get; set; }
+    }
+}
