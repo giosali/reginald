f27c4e2ce3c2c2142723c07120531de050bb3c98
giosali <70548443+giosali@users.noreply.github.com>
2022-08-14 14:57:55 -0500

    Rearrange methods

--- Reginald.Core/Helpers/BrushHelper.cs
+++ Reginald.Core/Helpers/BrushHelper.cs
@@ -6,6 +6,25 @@
 
     public static class BrushHelper
     {
+        public static Brush InvertBrush(Brush brush, bool makeReadable)
+        {
+            SolidColorBrush solidColorBrush = brush as SolidColorBrush;
+            byte r = (byte)~solidColorBrush.Color.R;
+            byte g = (byte)~solidColorBrush.Color.G;
+            byte b = (byte)~solidColorBrush.Color.B;
+
+            if (makeReadable)
+            {
+                // If the current RGB values create a grey background,
+                // return a black brush for black text on a grey background
+                r = r is > 110 and < 160 ? (byte)0 : r;
+                g = g is > 110 and < 160 ? (byte)0 : g;
+                b = b is > 110 and < 160 ? (byte)0 : b;
+            }
+
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
         public static Brush SolidColorBrushFromString(string expression)
         {
             return expression is null ? null : (SolidColorBrush)new BrushConverter().ConvertFromString(expression);
@@ -56,25 +75,6 @@
             }
 
             return false;
-        }
-
-        public static Brush InvertBrush(Brush brush, bool makeReadable)
-        {
-            SolidColorBrush solidColorBrush = brush as SolidColorBrush;
-            byte r = (byte)~solidColorBrush.Color.R;
-            byte g = (byte)~solidColorBrush.Color.G;
-            byte b = (byte)~solidColorBrush.Color.B;
-
-            if (makeReadable)
-            {
-                // If the current RGB values create a grey background,
-                // return a black brush for black text on a grey background
-                r = r is > 110 and < 160 ? (byte)0 : r;
-                g = g is > 110 and < 160 ? (byte)0 : g;
-                b = b is > 110 and < 160 ? (byte)0 : b;
-            }
-
-            return new SolidColorBrush(Color.FromRgb(r, g, b));
         }
     }
 }
