afc1ffa6667c1178b659cf50ae8549ccab6e7aec
giosali <70548443+giosali@users.noreply.github.com>
2022-08-14 13:25:32 -0500

    Update file

--- Reginald.Services/Hooks/KeyboardHook.cs
+++ Reginald.Services/Hooks/KeyboardHook.cs
@@ -5,37 +5,6 @@
     using System.Runtime.InteropServices;
     using static Reginald.Services.Hooks.NativeMethods;
 
-    /// <summary>
-    /// For more information, see the <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-mapvirtualkeya">Microsoft Documentation on virtual keys</see>.
-    /// </summary>
-    public enum MapVirtualKeyMapType : uint
-    {
-        /// <summary>
-        /// The uCode parameter is a virtual-key code and is translated into a scan code. If it is a virtual-key code that does not distinguish between left- and right-hand keys, the left-hand scan code is returned. If there is no translation, the function returns 0.
-        /// </summary>
-        MAPVK_VK_TO_VSC = 0x0,
-
-        /// <summary>
-        /// The uCode parameter is a scan code and is translated into a virtual-key code that does not distinguish between left- and right-hand keys. If there is no translation, the function returns 0.
-        /// </summary>
-        MAPVK_VSC_TO_VK = 0x1,
-
-        /// <summary>
-        /// The uCode parameter is a virtual-key code and is translated into an unshifted character value in the low order word of the return value. Dead keys (diacritics) are indicated by setting the top bit of the return value. If there is no translation, the function returns 0.
-        /// </summary>
-        MAPVK_VK_TO_CHAR = 0x2,
-
-        /// <summary>
-        /// The uCode parameter is a scan code and is translated into a virtual-key code that distinguishes between left- and right-hand keys. If there is no translation, the function returns 0.
-        /// </summary>
-        MAPVK_VSC_TO_VK_EX = 0x3,
-
-        /// <summary>
-        /// Windows Vista and later: The uCode parameter is a virtual-key code and is translated into a scan code. If it is a virtual-key code that does not distinguish between left- and right-hand keys, the left-hand scan code is returned. If the scan code is an extended scan code, the high byte of the uCode value can contain either 0xe0 or 0xe1 to specify the extended scan code. If there is no translation, the function returns 0.
-        /// </summary>
-        MAPVK_VK_TO_VSC_EX = 0x04,
-    }
-
     public class KeyboardHook : Hook
     {
         private readonly LowLevelKeyboardProc _proc;
