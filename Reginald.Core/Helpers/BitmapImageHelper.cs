419a5343ab5a028aa077c035bca2b0394686c3e3
giosali <70548443+giosali@users.noreply.github.com>
2022-08-14 14:36:50 -0500

    Refactor file

--- Reginald.Core/Helpers/BitmapImageHelper.cs
+++ Reginald.Core/Helpers/BitmapImageHelper.cs
@@ -7,41 +7,34 @@
     {
         public static BitmapImage FromUri(string uri, int width = 128, int height = 128)
         {
+            if (uri is null)
+            {
+                return null;
+            }
+
+            // Define a BitmapImage
+            BitmapImage icon = new();
+
+            // Begin initialization
+            icon.BeginInit();
+
+            // Assign properties
+            icon.CacheOption = BitmapCacheOption.OnLoad;
+            icon.DecodePixelWidth = width;
+            icon.DecodePixelHeight = height;
             try
             {
-                if (uri is null)
-                {
-                    throw new ArgumentNullException(uri);
-                }
-
-                // Define a BitmapImage
-                BitmapImage icon = new();
-
-                // Begin initialization
-                icon.BeginInit();
-
-                // Assign properties
-                icon.CacheOption = BitmapCacheOption.OnLoad;
-                icon.DecodePixelWidth = width;
-                icon.DecodePixelHeight = height;
                 icon.UriSource = new Uri(uri);
-
-                // End initialization
-                icon.EndInit();
-                icon.Freeze();
-                return icon;
             }
-            catch (Exception ex)
+            catch (UriFormatException)
             {
-                if (ex is UriFormatException or ArgumentNullException)
-                {
-                    return null;
-                }
-                else
-                {
-                    throw;
-                }
+                return null;
             }
+
+            // End initialization
+            icon.EndInit();
+            icon.Freeze();
+            return icon;
         }
     }
 }
