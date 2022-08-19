1623ec5d18f5891fed9e76a814ce7b7b6dbe2a1e
giosali <70548443+giosali@users.noreply.github.com>
2022-08-18 22:21:01 -0500

    Add InputProcessingEventArgs class

---
+++ Reginald.Data/Inputs/InputProcessingEventArgs.cs
@@ -0,0 +1,13 @@
+namespace Reginald.Data.Inputs
+{
+    using System;
+
+    public class InputProcessingEventArgs : EventArgs
+    {
+        public string CompleteInput { get; set; }
+
+        public bool Handled { get; set; }
+
+        public bool IsInputIncomplete { get; set; }
+    }
+}
