f55b920fc06b19854ed37cb4f631301ed7b66791
giosali <70548443+giosali@users.noreply.github.com>
2022-08-20 19:36:15 -0500

    Implement converter for strings and images

---
+++ Reginald/Converters/StringToImageSourceConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Reginald.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using Reginald.Services.Helpers;
+
+    [ValueConversion(typeof(string), typeof(ImageSource))]
+    internal class StringToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!uint.TryParse((string)value, out uint result))
+            {
+                return value;
+            }
+
+            return BitmapSourceHelper.GetStockIcon(result);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
