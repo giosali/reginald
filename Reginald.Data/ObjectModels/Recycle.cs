29b0fef0c07f3c3abafbcb80e97d6434b5405b68
giosali <70548443+giosali@users.noreply.github.com>
2022-08-22 00:31:31 -0500

    Add Recycle class

---
+++ Reginald.Data/ObjectModels/Recycle.cs
@@ -0,0 +1,38 @@
+namespace Reginald.Data.ObjectModels
+{
+    using Reginald.Data.Producers;
+    using Reginald.Data.Products;
+    using Newtonsoft.Json;
+    using Reginald.Data.Inputs;
+    using Reginald.Services.Utilities;
+
+    public class Recycle : ObjectModel, ISingleProducer<SearchResult>
+    {
+        [JsonProperty("isEnabled")]
+        public bool IsEnabled { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("requiresPrompt")]
+        public bool RequiresPrompt { get; set; }
+
+        public bool Check(string input)
+        {
+            return Key.Contains(input, System.StringComparison.OrdinalIgnoreCase);
+        }
+
+        public SearchResult Produce()
+        {
+            SearchResult result = new(Caption, Icon, Description);
+            result.EnterKeyPressed += OnEnterKeyPressed;
+            return result;
+        }
+
+        private void OnEnterKeyPressed(object sender, InputProcessingEventArgs e)
+        {
+            RecycleBin.Empty();
+            e.Handled = true;
+        }
+    }
+}
