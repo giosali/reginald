03ecc3e7a28f9f16165f1a059476c164fbdb290e
giosali <70548443+giosali@users.noreply.github.com>
2022-08-24 00:29:12 -0500

    Change return type from IEnumerable to List

--- Reginald.Services/Utilities/ProcessUtility.cs
+++ Reginald.Services/Utilities/ProcessUtility.cs
@@ -11,7 +11,7 @@
     {
         private static readonly HashSet<string> _systemWindows = new() { "Progman", "Shell_TrayWnd" };
 
-        public static IEnumerable<Process> GetTopLevelProcesses()
+        public static List<Process> GetTopLevelProcesses()
         {
             List<Process> processes = new();
             _ = EnumWindows(
