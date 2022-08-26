10a912894206b112500bb3b2f42e314f5d7b6162
giosali <70548443+giosali@users.noreply.github.com>
2022-08-26 11:23:24 -0500

    Add RegistryKeyWatcher

---
+++ Reginald.Services/Watchers/NativeMethods.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Reginald.Services.Watchers
+{
+    internal class NativeMethods
+    {
+        [Flags]
+        internal enum KeyAccessRight : uint
+        {
+            /// <summary>
+            /// Required to request change notifications for a registry key or for subkeys of a registry key.
+            /// </summary>
+            KEY_NOTIFY = 0x0010,
+        }
+
+        [Flags]
+        internal enum REG_NOTIFY_CHANGE : uint
+        {
+            /// <summary>
+            /// Notify the caller if a subkey is added or deleted.
+            /// </summary>
+            NAME = 0x00000001,
+        }
+        
+        [DllImport("advapi32.dll", SetLastError = true)]
+        internal static extern int RegCloseKey(IntPtr hKey);
+
+        [DllImport("advapi32.dll")]
+        internal static extern int RegNotifyChangeKeyValue(IntPtr hKey, bool watchSubtree, REG_NOTIFY_CHANGE notifyFilter, IntPtr hEvent, bool asynchronous);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
+        internal static extern int RegOpenKeyEx(IntPtr hKey, string subKey, int ulOptions, KeyAccessRight samDesired, out IntPtr hkResult);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+    }
+}
---
+++ Reginald.Services/Watchers/RegistryKeyWatcher.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Threading;
+using static Reginald.Services.Watchers.NativeMethods;
+
+namespace Reginald.Services.Watchers
+{
+    /// <summary>
+    /// An application can use handles to these keys as entry points to the registry.
+    /// </summary>
+    public enum RegistryHive : uint
+    {
+        /// <summary>
+        /// The HKEY_CURRENT_USER subtree contains the user profile for the user who is currently logged on to the computer. The user profile includes environment variables, personal program groups, desktop settings, network connections, printers, and application preferences. The data in the user profile is similar to the data stored in the Win.ini file in Windows 3.x.
+        /// </summary>
+        CurrentUser = 0x80000001,
+
+        /// <summary>
+        /// The HKEY_LOCAL_MACHINE subtree contains information about the local computer system, including hardware and operating system data, such as bus type, system memory, device drivers, and startup control parameters.
+        /// </summary>
+        LocalMachine = 0x80000002,
+    }
+
+    public class RegistryKeyWatcher
+    {
+        private const uint INFINITE = 0xFFFFFFFF;
+
+        private Thread _thread;
+
+        [Flags]
+        private enum WaitResult : uint
+        {
+            /// <summary>
+            /// The function has failed. To get extended error information, call GetLastError.
+            /// </summary>
+            WAIT_FAILED = 0xFFFFFFFF,
+        }
+
+        public RegistryKeyWatcher(RegistryHive registryHive, string registrySubKey)
+        {
+            RegistryKey = new((uint)registryHive);
+            RegistrySubKey = registrySubKey;
+            _thread = new(WatchRegistryKey);
+            _thread.IsBackground = true;
+        }
+
+        public string RegistrySubKey { get; set; }
+
+        private IntPtr RegistryKey { get; set; }
+
+        public event EventHandler<EventArgs> RegistryKeyChanged;
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+
+        private void WatchRegistryKey()
+        {
+            IntPtr hEvent = CreateEvent(IntPtr.Zero, true, false, "RegistryKeyWatcherEvent");
+            if (hEvent == IntPtr.Zero)
+            {
+                return;
+            }
+
+            if (RegOpenKeyEx(RegistryKey, RegistrySubKey, 0, KeyAccessRight.KEY_NOTIFY, out IntPtr hKey) != 0)
+            {
+                return;
+            }
+
+            while (true)
+            {
+                switch (RegNotifyChangeKeyValue(hKey, false, REG_NOTIFY_CHANGE.NAME, hEvent, true))
+                {
+                    case 0:
+                        if (WaitForSingleObject(hEvent, INFINITE) == (uint)WaitResult.WAIT_FAILED)
+                        {
+                            return;
+                        }
+
+                        RegistryKeyChanged?.Invoke(this, new EventArgs());
+                        break;
+                    default:
+                        _ = RegCloseKey(hKey);
+                        CloseHandle(hEvent);
+                        return;
+                }
+            }
+        }
+    }
+}
