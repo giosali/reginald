ce35945a3291d0cbab4cd9388afbfcc265a77918
giosali <70548443+giosali@users.noreply.github.com>
2022-08-27 11:49:08 -0500

    Add FeaturesViewModel and FeaturesView

---
+++ Reginald/ViewModels/FeaturesViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace Reginald.ViewModels
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using Caliburn.Micro;
+
+    internal class FeaturesViewModel : Conductor<object>
+    {
+        public async void ListBoxItem_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Type type = (e.Source as ListBoxItem).Tag switch
+            {
+                "WebQueries" => typeof(WebQueriesViewModel),
+                "YourWebQueries" => typeof(YourWebQueriesViewModel),
+                _ => null,
+            };
+            if (IoC.GetInstance(type, null) is IScreen screen && !screen.IsActive)
+            {
+                await ActivateItemAsync(screen);
+            }
+        }
+
+        public void ScrollViewer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+            {
+                _ = scrollViewer.Focus();
+            }
+        }
+
+        public void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+            {
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta);
+            }
+
+            e.Handled = true;
+        }
+
+        protected override Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            _ = ActivateItemAsync(IoC.GetInstance(typeof(WebQueriesViewModel), null), cancellationToken);
+            return base.OnActivateAsync(cancellationToken);
+        }
+    }
+}
