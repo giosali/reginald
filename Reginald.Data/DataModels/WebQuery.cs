d1269d18e8cc138b057611bdfd19f1d3064f599d
giosali <70548443+giosali@users.noreply.github.com>
2022-08-27 22:22:55 -0500

    Add static property FileName

--- Reginald.Data/DataModels/WebQuery.cs
+++ Reginald.Data/DataModels/WebQuery.cs
@@ -10,6 +10,8 @@
 
     public class WebQuery : DataModel, ISingleProducer<SearchResult>
     {
+        public static string FileName = "WebQueries.json";
+
         public static string UserFileName = "YourWebQueries.json";
 
         private string _keyInput;
