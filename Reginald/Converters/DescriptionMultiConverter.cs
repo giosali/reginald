9e240c7529e38372294d19100ca729550ef516ee
giosali <70548443+giosali@users.noreply.github.com>
2022-08-29 11:19:52 -0500

    Add description converter for datagrids

---
+++ Reginald/Converters/DescriptionMultiConverter.cs
@@ -0,0 +1,28 @@
+namespace Reginald.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Input;
+
+    public class DescriptionMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string description = (string)values[0];
+            if (!string.IsNullOrEmpty(description))
+            {
+                return description;
+            }
+
+            string descriptionFormat = (string)values[1];
+            string placeholder = (string)values[2];
+            return string.Format(descriptionFormat, placeholder);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
