2898775bdd86e2bc99a546a9fc68559a41148acb
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 18:33:59 -0500

    Add Calculator view and view model

---
+++ Reginald/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.ViewModels
+{
+    using Reginald.Services;
+
+    internal class CalculatorViewModel : ItemScreen
+    {
+        public CalculatorViewModel(DataModelService dms)
+            : base("Features > Calculator")
+        {
+            DataModelService = dms;
+        }
+
+        public DataModelService DataModelService { get; set; }
+    }
+}
