98ddc685ba9ddd28b1a2cf9b5e1639df726a2a57
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 18:55:16 -0500

    Add view and view model for applications

---
+++ Reginald/ViewModels/ApplicationsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.ViewModels
+{
+    using Reginald.Services;
+
+    internal class ApplicationsViewModel : ItemScreen
+    {
+        public ApplicationsViewModel(DataModelService dms)
+            : base("Features > Applications")
+        {
+            DataModelService = dms;
+        }
+
+        public DataModelService DataModelService { get; set; }
+    }
+}
