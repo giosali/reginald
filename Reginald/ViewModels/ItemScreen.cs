cae9ffeec00d6db755006e45f96ca699ffe8c520
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 18:33:22 -0500

    Add ItemScreen

--- Reginald/ViewModels/ItemScreen.cs
+++ Reginald/ViewModels/ItemScreen.cs
@@ -1,8 +1,31 @@
 ﻿namespace Reginald.ViewModels
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
     using Caliburn.Micro;
+    using Reginald.Messages;
+    using Reginald.Services;
 
-    internal abstract class ItemScreen<T> : Screen
+    internal abstract class ItemScreen : Screen
     {
+        private readonly string _pageName;
+
+        public ItemScreen(string pageName)
+        {
+            _pageName = pageName;
+        }
+
+        public void Include_Click(object sender, RoutedEventArgs e)
+        {
+            IoC.Get<DataModelService>().Settings.Save();
+        }
+
+        protected override Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            IEventAggregator eventAggregator = IoC.Get<IEventAggregator>();
+            _ = eventAggregator.PublishOnUIThreadAsync(new UpdatePageMessage(_pageName), cancellationToken);
+            return base.OnActivateAsync(cancellationToken);
+        }
     }
 }
