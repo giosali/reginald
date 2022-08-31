0e596926cdca21ae68905d7ac5ad3300e4d582b5
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 19:25:20 -0500

    Add MicrosoftSettingsView and MicrosoftSettingsViewModel

---
+++ Reginald/ViewModels/MicrosoftSettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.ViewModels
+{
+    using Reginald.Services;
+
+    internal class MicrosoftSettingsViewModel : ItemScreen
+    {
+        public MicrosoftSettingsViewModel(DataModelService dms)
+            : base("Features > Microsoft Settings")
+        {
+            DataModelService = dms;
+        }
+
+        public DataModelService DataModelService { get; set; }
+    }
+}
