862be212fa9543ab65c44ee5b14a8418760eed2d
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 21:23:59 -0500

    Remove unnecessary usings

--- Reginald/Views/TimerView.xaml.cs
+++ Reginald/Views/TimerView.xaml.cs
@@ -1,22 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Reginald.Views
+﻿namespace Reginald.Views
 {
+    using System.Windows.Controls;
+
     /// <summary>
-    /// Interaction logic for TimerView.xaml
+    /// Interaction logic for TimerView.xaml.
     /// </summary>
     public partial class TimerView : UserControl
     {
