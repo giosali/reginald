789eb1b7ce5f93b8701070f938527188741fe488
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 23:16:53 -0500

    Add RecycleView and RecycleViewModel

---
+++ Reginald/ViewModels/RecycleViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.ViewModels
+{
+    using Reginald.Services;
+
+    internal class RecycleViewModel : ItemScreen
+    {
+        public RecycleViewModel(DataModelService dms)
+            : base("Features > Recycle")
+        {
+            DataModelService = dms;
+        }
+
+        public DataModelService DataModelService { get; set; }
+    }
+}
---
+++ Reginald/Views/RecycleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for RecycleView.xaml.
+    /// </summary>
+    public partial class RecycleView : UserControl
+    {
+        public RecycleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
