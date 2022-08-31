81e4997ff5c87c7a48a7b1f2f18b1721ef8d5269
giosali <70548443+giosali@users.noreply.github.com>
2022-08-30 19:13:23 -0500

    Add LinkView and LinkViewModel

---
+++ Reginald/ViewModels/LinkViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Reginald.ViewModels
+{
+    using Reginald.Services;
+
+    internal class LinkViewModel : ItemScreen
+    {
+        public LinkViewModel(DataModelService dms)
+            : base("Features > Link")
+        {
+            DataModelService = dms;
+        }
+
+        public DataModelService DataModelService { get; set; }
+    }
+}
