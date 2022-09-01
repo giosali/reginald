2425bb9beff60b9d92fe10057e704e97c72c5ff8
giosali <70548443+giosali@users.noreply.github.com>
2022-08-31 20:11:22 -0500

    Add TextExpansion class

---
+++ Reginald.Data/DataModels/TextExpansion.cs
@@ -0,0 +1,21 @@
+namespace Reginald.Data.DataModels
+{
+    using Newtonsoft.Json;
+
+    public class TextExpansion
+    {
+        public const string FileName = "TextExpansions.json";
+
+        public TextExpansion(string trigger, string replacement)
+        {
+            Trigger = trigger;
+            Replacement = replacement;
+        }
+
+        [JsonProperty("replacement")]
+        public string Replacement { get; set; }
+
+        [JsonProperty("trigger")]
+        public string Trigger { get; set; }
+    }
+}
