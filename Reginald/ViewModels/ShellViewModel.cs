e0649ee76431fe5cc711e2b5cedff74300368c1f
giosali <70548443+giosali@users.noreply.github.com>
2022-08-31 19:06:02 -0500

    Refactor code

--- Reginald/ViewModels/ShellViewModel.cs
+++ Reginald/ViewModels/ShellViewModel.cs
@@ -15,13 +15,13 @@
 
     internal class ShellViewModel : Conductor<object>
     {
-        private readonly IWindowManager _windowManager;
+        private readonly ClipboardManagerPopupViewModel _clipboardManagerPopupViewModel;
 
         private readonly MainViewModel _mainViewModel;
 
-        private readonly ClipboardManagerPopupViewModel _clipboardManagerPopupViewModel;
-
         private readonly TextExpansionManager _textExpansionManager;
+
+        private readonly IWindowManager _windowManager;
 
         private bool _isEnabled = true;
 
@@ -52,8 +52,6 @@
 
         public DataModelService DMS { get; set; }
 
-        public string ToolTipText { get; set; }
-
         public bool IsEnabled
         {
             get => _isEnabled;
@@ -64,44 +62,52 @@
             }
         }
 
-        public async void SearchWindowHotkeyBinding_Pressed(object sender, HotkeyEventArgs e)
-        {
-            if (IsEnabled)
-            {
-                if (_mainViewModel.IsActive)
-                {
-                    _mainViewModel.Hide();
-                }
-                else
-                {
-                    Dictionary<string, object> settings = new();
-                    settings.Add("Placement", PlacementMode.Absolute);
-                    settings.Add("HorizontalOffset", (SystemParameters.FullPrimaryScreenWidth / 2) - (DMS.Theme.MainWidth / 2));
-                    settings.Add("VerticalOffset", (SystemParameters.FullPrimaryScreenHeight / 2 * 0.325) - (DMS.Theme.MainHeight / 4));
-                    await new WindowManager().ShowPopupAsync(_mainViewModel, settings: settings);
-                }
-            }
-        }
+        public string ToolTipText { get; set; }
 
         public async void ClipboardManagerPopupHotkeyBinding_Pressed(object sender, HotkeyEventArgs e)
         {
             if (DMS.Settings.IsClipboardManagerEnabled)
             {
-                if (_clipboardManagerPopupViewModel.IsActive)
-                {
-                    _clipboardManagerPopupViewModel.Hide();
-                }
-                else
-                {
-                    Dictionary<string, object> settings = new();
-                    settings.Add("PopupAnimation", PopupAnimation.Fade);
-
-                    // StaysOpen must be true for the popup to remain draggable
-                    // when sending its handle a WM_NCLBUTTONDOWN message.
-                    settings.Add("StaysOpen", true);
-                    await _windowManager.ShowPopupAsync(_clipboardManagerPopupViewModel, settings: settings);
-                }
+                return;
             }
+
+
+            if (_clipboardManagerPopupViewModel.IsActive)
+            {
+                _clipboardManagerPopupViewModel.Hide();
+                return;
+            }
+           
+            Dictionary<string, object> settings = new()
+            {
+                { "PopupAnimation", PopupAnimation.Fade },
+                // StaysOpen must be true for the popup to remain draggable
+                // when sending its handle a WM_NCLBUTTONDOWN message.
+                { "StaysOpen", true },
+            };
+            await _windowManager.ShowPopupAsync(_clipboardManagerPopupViewModel, settings: settings);
+        }
+
+        public async void MainHotkeyBinding_Pressed(object sender, HotkeyEventArgs e)
+        {
+            if (!IsEnabled)
+            {
+                return;
+            }
+
+            if (_mainViewModel.IsActive)
+            {
+                _mainViewModel.Hide();
+                return;
+            }
+            
+            Dictionary<string, object> settings = new()
+            {
+                { "Placement", PlacementMode.Absolute },
+                { "HorizontalOffset", (SystemParameters.FullPrimaryScreenWidth / 2) - (DMS.Theme.MainWidth / 2) },
+                { "VerticalOffset", (SystemParameters.FullPrimaryScreenHeight / 2 * 0.325) - (DMS.Theme.MainHeight / 4) },
+            };
+            await new WindowManager().ShowPopupAsync(_mainViewModel, settings: settings);
         }
 
         public async void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -109,11 +115,9 @@
             switch ((e.Source as MenuItem).Tag)
             {
                 case "Settings":
-                    object instance = IoC.GetInstance(typeof(SettingsViewModel), null);
-                    await _windowManager.ShowWindowAsync(instance);
+                    await _windowManager.ShowWindowAsync(IoC.GetInstance(typeof(SettingsViewModel), null));
                     break;
-
-                case "LaunchOnStartup":
+                case "RunAtStartup":
                     // Checks if the shortcut already exists by checking if
                     // TryCreateShortcut returns false.
                     // If it returns false, the shortcut gets deleted.
@@ -124,7 +128,6 @@
 
                     DMS.Settings.Save();
                     break;
-
                 case "Exit":
                     Application.Current.Shutdown();
                     break;
