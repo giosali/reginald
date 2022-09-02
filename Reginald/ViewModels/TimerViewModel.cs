0578e4bbb276d8fb0d8e9cf35a8b788bb27186cc
giosali <70548443+giosali@users.noreply.github.com>
2022-09-02 01:27:23 -0500

    Add timers keyword and links

--- Reginald/ViewModels/TimerViewModel.cs
+++ Reginald/ViewModels/TimerViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace Reginald.ViewModels
 {
+    using System.Windows;
+    using System.Windows.Controls;
     using Reginald.Services;
+    using Reginald.Services.Utilities;
 
     internal class TimerViewModel : ItemScreen
     {
@@ -11,5 +14,10 @@
         }
 
         public DataModelService DataModelService { get; set; }
+
+        public void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessUtility.GoTo((sender as Button).Tag.ToString());
+        }
     }
 }
