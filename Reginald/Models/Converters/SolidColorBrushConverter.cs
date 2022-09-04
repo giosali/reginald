124ee3905036e2e7cd73959cdaeca1b771430af5
giosali <70548443+giosali@users.noreply.github.com>
2022-09-04 13:30:00 -0500

    Add SolidColorBrushConverter

---
+++ Reginald/Models/Converters/SolidColorBrushConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace Reginald.Models.Converters
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Media;
+    using Newtonsoft.Json;
+
+    internal class SolidColorBrushConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(SolidColorBrush);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is not string value)
+            {
+                return null;
+            }
+
+            SolidColorBrush brush = new();
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                brush = new BrushConverter().ConvertFromString(value) as SolidColorBrush;
+            });
+            return brush;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value);
+        }
+    }
+}
