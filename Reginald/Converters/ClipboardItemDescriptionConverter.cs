692a78c3f68cd0ee6bc8b774371c493197a1d343
giosali <70548443+giosali@users.noreply.github.com>
2022-09-05 14:53:20 -0500

    Add ClipboardItemDescriptionConverter to improve performance for long selections

---
+++ Reginald/Converters/ClipboardItemDescriptionConverter.cs
@@ -0,0 +1,27 @@
+namespace Reginald.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(string), typeof(string))]
+    public class ClipboardItemDescriptionConverter : IValueConverter
+    {
+        private const int MaxLength = 15000;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string str)
+            {
+                return null;
+            }
+
+            return str.Length <= MaxLength ? str : str[..MaxLength] + "\nTRUNCATED";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
