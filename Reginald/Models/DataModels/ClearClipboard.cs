9d0926d5cc473075fe617835a08114566ace2b9e
giosali <70548443+giosali@users.noreply.github.com>
2022-09-06 17:24:56 -0500

    Add ClearClipboard class

---
+++ Reginald/Models/DataModels/ClearClipboard.cs
@@ -0,0 +1,42 @@
+﻿namespace Reginald.Models.DataModels
+{
+    using Caliburn.Micro;
+    using Newtonsoft.Json;
+    using Reginald.Core.Extensions;
+    using Reginald.Models.Inputs;
+    using Reginald.Models.Producers;
+    using Reginald.Models.Products;
+    using Reginald.ViewModels;
+
+    internal class ClearClipboard : DataModel, ISingleProducer<SearchResult>
+    {
+        [JsonProperty("isEnabled")]
+        public bool IsEnabled { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        public bool Check(string input)
+        {
+            if (!IsEnabled)
+            {
+                return false;
+            }
+
+            return Key.ContainsPhrase(input);
+        }
+
+        public SearchResult Produce()
+        {
+            SearchResult result = new(Caption, IconPath, Description);
+            result.EnterKeyPressed += OnEnterKeyPressed;
+            return result;
+        }
+
+        private void OnEnterKeyPressed(object sender, InputProcessingEventArgs e)
+        {
+            e.Handled = true;
+            IoC.Get<ClipboardManagerPopupViewModel>().Clear();
+        }
+    }
+}
