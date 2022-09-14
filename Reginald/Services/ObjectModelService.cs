dcff7b651702b5c7552cf0afe7843a040ae8c44d
giosali <70548443+giosali@users.noreply.github.com>
2022-09-13 23:10:23 -0500

    Add methods for indexing user's file system and keeping track of add, removed, and renamed file system entries

--- Reginald/Services/ObjectModelService.cs
+++ Reginald/Services/ObjectModelService.cs
@@ -1,7 +1,10 @@
 ﻿namespace Reginald.Services
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
     using Caliburn.Micro;
     using Reginald.Models.ObjectModels;
     using Reginald.Models.Producers;
@@ -10,8 +13,12 @@
 
     internal class ObjectModelService
     {
+        private readonly string _appDataPath = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).FullName;
+
 #pragma warning disable IDE0052
-        private readonly RegistryKeyWatcher[] _watchers;
+        private readonly FileSystemWatcher[] _fileSystemWatchers;
+
+        private readonly RegistryKeyWatcher[] _registryKeyWatchers;
 #pragma warning restore IDE0052
 
         public ObjectModelService()
@@ -24,8 +31,28 @@
             localMachine.RegistryKeyChanged += OnRegistryKeyChanged;
             RegistryKeyWatcher currentUser = new(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
             currentUser.RegistryKeyChanged += OnRegistryKeyChanged;
-            _watchers = new RegistryKeyWatcher[] { localMachine64Bit, localMachine, currentUser };
+            _registryKeyWatchers = new RegistryKeyWatcher[] { localMachine64Bit, localMachine, currentUser };
+
+            FileSystemWatcher fsWatcher = new(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            fsWatcher.NotifyFilter = NotifyFilters.Attributes
+                                   | NotifyFilters.CreationTime
+                                   | NotifyFilters.DirectoryName
+                                   | NotifyFilters.FileName
+                                   | NotifyFilters.LastAccess
+                                   | NotifyFilters.LastWrite
+                                   | NotifyFilters.Security
+                                   | NotifyFilters.Size;
+            fsWatcher.Created += OnCreated;
+            fsWatcher.Deleted += OnDeleted;
+            fsWatcher.Renamed += OnRenamed;
+            fsWatcher.IncludeSubdirectories = true;
+            fsWatcher.EnableRaisingEvents = true;
+            _fileSystemWatchers = new FileSystemWatcher[] { fsWatcher };
+            Thread th = new(SetFileSystemEntries);
+            th.Start();
         }
+
+        public ConcurrentDictionary<string, FileSystemEntry> FileSystemEntries { get; set; } = new();
 
         public ISingleProducer<SearchResult>[] SingleProducers { get; private set; }
 
@@ -40,9 +67,76 @@
             SingleProducers = singleProducers.ToArray();
         }
 
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Created:
+                    string fullPath = e.FullPath;
+                    if (fullPath.Contains(_appDataPath) || Path.GetFileName(fullPath).StartsWith('.'))
+                    {
+                        break;
+                    }
+
+                    FileSystemEntries[fullPath] = new FileSystemEntry(fullPath);
+                    break;
+            }
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Deleted:
+                    string fullPath = e.FullPath;
+                    if (fullPath.Contains(_appDataPath))
+                    {
+                        break;
+                    }
+
+                    _ = FileSystemEntries.TryRemove(fullPath, out _);
+                    break;
+            }
+        }
+
         private void OnRegistryKeyChanged(object sender, EventArgs e)
         {
             SetSingleProducers();
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Renamed:
+                    string oldFullPath = e.OldFullPath;
+                    if (FileSystemEntries.TryRemove(oldFullPath, out FileSystemEntry entry))
+                    {
+                        FileSystemEntries[e.FullPath] = entry;
+                    }
+
+                    break;
+            }
+        }
+
+        private void SetFileSystemEntries()
+        {
+            EnumerationOptions options = new()
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true,
+            };
+            foreach (string entry in Directory.EnumerateFileSystemEntries(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "*", options))
+            {
+                // Skips file system entry if it's located in the user's %APPDATA%
+                // or if its file name begins with a period.
+                if (entry.StartsWith(_appDataPath) || Path.GetFileName(entry).StartsWith('.'))
+                {
+                    continue;
+                }
+
+                FileSystemEntries[entry] = new FileSystemEntry(entry);
+            }
         }
     }
 }
