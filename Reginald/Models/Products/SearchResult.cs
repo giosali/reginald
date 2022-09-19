afd8b4b2c18bc24f2af409565192c05ab0937fab
giosali <70548443+giosali@users.noreply.github.com>
2022-09-19 15:50:14 -0500

    Change implementation of GetHashCode

--- Reginald/Models/Products/SearchResult.cs
+++ Reginald/Models/Products/SearchResult.cs
@@ -71,22 +71,22 @@
 
         public static bool operator ==(SearchResult a, SearchResult b)
         {
-            return a is not null && b is not null && a.Id == b.Id;
+            return a is not null && b is not null && a.Id == b.Id && a.Description == b.Description;
         }
 
         public static bool operator !=(SearchResult a, SearchResult b)
         {
-            return a is not null && b is not null && a.Id != b.Id;
+            return a is not null && b is not null && a.Id != b.Id && a.Description != b.Description;
         }
 
         public override bool Equals(object obj)
         {
-            return obj is SearchResult result && Id == result.Id;
+            return obj is SearchResult result && Id == result.Id && Description == result.Description;
         }
 
         public override int GetHashCode()
         {
-            return Id;
+            return Id ^ Description.GetHashCode();
         }
 
         public override void PressAlt(InputProcessingEventArgs e)
