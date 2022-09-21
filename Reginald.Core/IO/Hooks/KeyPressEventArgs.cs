5084a888f07b7eab4c2a06a9c9d40917eae232b7
giosali <70548443+giosali@users.noreply.github.com>
2022-09-21 13:51:19 -0500

    Rearrange properties

--- Reginald.Core/IO/Hooks/KeyPressEventArgs.cs
+++ Reginald.Core/IO/Hooks/KeyPressEventArgs.cs
@@ -5,20 +5,20 @@
 
     public class KeyPressEventArgs : EventArgs
     {
-        private readonly bool _isModifierPressed;
-
-        private readonly bool _isVolumeKeyPressed;
-
         private readonly bool _isCapsLockPressed;
 
+        private readonly bool _isModifierPressed;
+
         private readonly bool _isPrintScreenPressed;
+
+        private readonly bool _isVolumeKeyPressed;
 
         public KeyPressEventArgs(int vkCode, bool isDown)
         {
             IsDown = isDown;
             VirtualKeyCode = vkCode;
-            Key key = Key = KeyInterop.KeyFromVirtualKey(vkCode);
-            switch (key)
+            Key = KeyInterop.KeyFromVirtualKey(vkCode);
+            switch (Key)
             {
                 case Key.LeftAlt:
                 case Key.RightAlt:
@@ -30,31 +30,28 @@
                 case Key.RWin:
                     _isModifierPressed = true;
                     break;
-
                 case Key.VolumeDown:
                 case Key.VolumeMute:
                 case Key.VolumeUp:
                     _isVolumeKeyPressed = true;
                     break;
-
                 case Key.CapsLock:
                     _isCapsLockPressed = true;
                     break;
-
                 case Key.PrintScreen:
                     _isPrintScreenPressed = true;
                     break;
             }
         }
 
-        public int VirtualKeyCode { get; set; }
-
-        public Key Key { get; set; }
-
         public bool IsDown { get; set; }
 
         public bool IsHotkeyPressed { get; set; }
 
         public bool IsImportantKeyPressed => IsHotkeyPressed || _isModifierPressed || _isVolumeKeyPressed || _isCapsLockPressed || _isPrintScreenPressed;
+
+        public Key Key { get; set; }
+
+        public int VirtualKeyCode { get; set; }
     }
 }
