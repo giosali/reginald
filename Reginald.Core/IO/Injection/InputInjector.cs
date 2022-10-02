9a047c03aaf945a4373521afcf4a6bc4c37c90c3
giosali <70548443+giosali@users.noreply.github.com>
2022-10-01 22:46:03 -0500

    Remove unused methods

--- Reginald.Core/IO/Injection/InputInjector.cs
+++ Reginald.Core/IO/Injection/InputInjector.cs
@@ -1,6 +1,5 @@
 ﻿namespace Reginald.Core.IO.Injection
 {
-    using System;
     using static Reginald.Core.IO.Injection.NativeMethods;
 
     public static class InputInjector
@@ -8,18 +7,6 @@
         public static void InjectKeyboardInput(InjectedInputKeyboardInfo info)
         {
             _ = SendInput((uint)info.Inputs.Length, info.Inputs, INPUT.Size);
-        }
-
-        public static bool SendKeyDown(IntPtr hWnd, int vkCode)
-        {
-            // 0x0100 = WM_KEYDOWN.
-            return PostMessage(hWnd, 0x0100, vkCode, 0);
-        }
-
-        public static bool SendKeyUp(IntPtr hWnd, int vkCode)
-        {
-            // 0x0101 = WM_KEYUP.
-            return PostMessage(hWnd, 0x0101, vkCode, 0);
         }
     }
 }
