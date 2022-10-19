f866cc6962ca129df6466f20f0b261438f906140
giosali <70548443+giosali@users.noreply.github.com>
2022-10-18 22:39:24 -0500

    Add routing

--- reginaldapp/src/index.jsx
+++ reginaldapp/src/index.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+// import App from './App';
+import Error from './components/Error';
+import Features from './components/Features';
+import Home from './components/Home';
+import Root from './components/Root';
+
+const router = createBrowserRouter([
+  {
+    children: [
+      {
+        element: <Home />,
+        path: '',
+      },
+      {
+        path: 'features',
+        element: <Features />,
+      },
+    ],
+    element: <Root />,
+    errorElement: <Error />,
+    path: 'reginald',
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
