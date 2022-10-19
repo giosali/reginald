b950102b67d5e2ea748a97f8df04c1045da3bedb
giosali <70548443+giosali@users.noreply.github.com>
2022-10-18 23:46:16 -0500

    Add underline to Link components

--- reginaldapp/src/components/Root.jsx
+++ reginaldapp/src/components/Root.jsx
@@ -10,16 +10,24 @@ function Root() {
     <div className="Root">
       <nav className="nav">
         <ul className="nav__list">
-          <Link className="nav__item" to="/reginald">
-            <img
-              alt="Reginald logo"
-              className="nav__logo"
-              src={Logo}
-            />
-          </Link>
-          <Link className="nav__item" to="/reginald/features">Features</Link>
-          <Link className="nav__item" to="/reginald/github">GitHub</Link>
-          <Link className="nav__item" to="/reginald/download">Download</Link>
+          <li className="nav__item">
+            <Link className="nav__link" to="/reginald">
+              <img
+                alt="Reginald logo"
+                className="nav__logo"
+                src={Logo}
+              />
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="/reginald/features">Features</Link>
+          </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="/reginald/github">GitHub</Link>
+          </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="/reginald/download">Download</Link>
+          </li>
         </ul>
       </nav>
       <Outlet />
