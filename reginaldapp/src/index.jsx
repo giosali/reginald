e14ef3115648320945cd4636551b7058c1483995
giosali <70548443+giosali@users.noreply.github.com>
2022-10-18 20:30:49 -0500

    Rename file

--- reginaldapp/src/index.jsx
+++ reginaldapp/src/index.jsx
@@ -6,7 +6,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // eslint-disable-next-line react/jsx-filename-extension
   <React.StrictMode>
     <App />
   </React.StrictMode>,
