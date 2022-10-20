82db19e827006847cf96598eb3726578b01c7727
giosali <70548443+giosali@users.noreply.github.com>
2022-10-19 22:51:15 -0500

    Add mobile-based styling

--- reginaldapp/src/components/Root.jsx
+++ reginaldapp/src/components/Root.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link, Outlet } from 'react-router-dom';
 
 import '../stylesheets/Root.css';
+import HamburgerMenu from './HamburgerMenu';
 import Logo from '../images/logo.png';
 
 function Root() {
+  const [isHamburgerMenuActive, setIsHamburgerMenuActive] = useState(false);
+  const handleClick = () => {
+    setIsHamburgerMenuActive(!isHamburgerMenuActive);
+  };
+
   return (
     <div className="Root">
       <nav className="nav">
@@ -20,13 +26,18 @@ function Root() {
             </Link>
           </li>
           <li className="nav__item">
-            <Link className="nav__link" to="/reginald/features">Features</Link>
-          </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/reginald/github">GitHub</Link>
-          </li>
-          <li className="nav__item">
-            <Link className="nav__link" to="/reginald/download">Download</Link>
+            <HamburgerMenu onClick={handleClick} />
+            <button
+              aria-label="close"
+              className={isHamburgerMenuActive ? 'nav__hamburger-menu-close nav__hamburger-menu-close--active' : 'nav__hamburger-menu-close'}
+              onClick={handleClick}
+              type="button"
+            />
+            <div className={isHamburgerMenuActive ? 'nav__hamburger-menu-links nav__hamburger-menu-links--active' : 'nav__hamburger-menu-links'}>
+              <Link className="nav__link" to="/reginald/features">Features</Link>
+              <Link className="nav__link" to="/reginald/github">GitHub</Link>
+              <Link className="nav__link" to="/reginald/download">Download</Link>
+            </div>
           </li>
         </ul>
       </nav>
