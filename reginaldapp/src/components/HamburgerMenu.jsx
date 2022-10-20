9c80904d9b39a0865c4f01d9a9264e7cdd285aaf
giosali <70548443+giosali@users.noreply.github.com>
2022-10-19 19:28:06 -0500

    Add HamburgerMenu component

---
+++ reginaldapp/src/components/HamburgerMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../stylesheets/HamburgerMenu.css';
+
+function HamburgerMenu() {
+  return (
+    <div className="HamburgerMenu">
+      <div className="HamburgerMenu__line" />
+      <div className="HamburgerMenu__line" />
+      <div className="HamburgerMenu__line" />
+    </div>
+  );
+}
+
+export default HamburgerMenu;
