21d86854aea885b7481150b9a990c531a63754fa
giosali <70548443+giosali@users.noreply.github.com>
2022-10-19 19:31:49 -0500

    Change Home component content

--- reginaldapp/src/components/Home.jsx
+++ reginaldapp/src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import '../stylesheets/Home.css';
+import Logo from '../images/logo.png';
+
 function Home() {
   return (
-    <p style={{ color: 'white' }}>This is the home page</p>
+    <div className="Home">
+      <div className="hero">
+        <img
+          alt="Reginald logo"
+          className="hero__image"
+          src={Logo}
+        />
+        <h1 className="hero__title">Reginald</h1>
+        <p className="hero__description">A general productivity application designed for Windows and inspired by Alfred</p>
+      </div>
+    </div>
   );
 }
 
