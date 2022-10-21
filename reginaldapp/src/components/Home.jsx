a38ef05358fefc7deb82b753dcea159a31797828
giosali <70548443+giosali@users.noreply.github.com>
2022-10-20 22:24:33 -0500

    Implement GitHubButton component

--- reginaldapp/src/components/Home.jsx
+++ reginaldapp/src/components/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../stylesheets/Home.css';
+import GitHubButton from './GitHubButton';
 import Logo from '../images/logo.png';
 
 function Home() {
+  const [downloadUrl, setDownloadUrl] = useState('https://github.com/giosali/reginald/releases');
+
+  useEffect(() => {
+    const ep = 'https://api.github.com/repos/giosali/reginald/releases/latest';
+    fetch(ep, {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setDownloadUrl(data.assets[0].browser_download_url))
+      .catch((error) => console.log(error.message));
+  }, []);
+
   return (
     <div className="Home">
       <div className="hero">
@@ -14,6 +29,19 @@ function Home() {
         />
         <h1 className="hero__title">Reginald</h1>
         <p className="hero__description">A general productivity application designed for Windows and inspired by Alfred</p>
+        <div className="hero__github-buttons">
+          <GitHubButton
+            href="https://github.com/giosali/reginald"
+            icon="octicon-star"
+            repo="giosali/reginald"
+            type="Star"
+          />
+          <GitHubButton
+            href={downloadUrl}
+            repo="giosali/reginald"
+            type="Download"
+          />
+        </div>
       </div>
     </div>
   );
